995b12379915830eafb1ddcb57bad2cf5c3af75d
Daniela1026 <cartagenad49@gmail.com>
2022-09-15 08:02:07 -0700

    creates a binary tree node

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef _SEARCH_ALGOS_H_
 #define _SEARCH_ALGOS_H_
 
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -25,6 +28,7 @@ typedef struct binary_tree_s avl_t;
 
 typedef struct binary_tree_s heap_t;
 
+void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
