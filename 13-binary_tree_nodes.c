576e9c86edc91f60bbabcb5fa60de7e977cf65cf
Daniela1026 <cartagenad49@gmail.com>
2022-09-16 11:58:16 -0700

    1 child in a binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,19 +9,14 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t left_l;
-	size_t right_l;
-	size_t total = 0;
+	size_t nodes = 0;
 
-	if (!tree || (!tree->right && !tree->left))
-	    return (0);
+	if (tree == NULL)
+		return (0);
 
-	if (tree->left || tree->right)
-	{
-		left_l = binary_tree_nodes(tree->left) + 1;
-		right_l = binary_tree_nodes(tree->right) + 1;
-		total = left_l + right_l - 1;
-	}
+	nodes += (tree->left || tree->right ? 1 : 0);
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
 
-	return (total);
+	return (nodes);
 }
